9db88a080b783efa6d9cd1132a6bb1b1a24669c0
ahmede460 <ahmad.jaffar97@gmail.com>
2024-10-07 19:29:08 +0300

    add sound effects

--- script.js
+++ script.js
@@ -6,18 +6,21 @@ let userChoices = []
 let computerChoices = []
 let lose = false
 const GAMECOLORS = ["green", "red", "yellow", "blue"]
-level = 1
+let level = 1
+const audio = {
+    "red": new Audio('./soundeffects/red.mp3'),
+    "green": new Audio('./soundeffects/green.mp3'),
+    "yellow": new Audio('./soundeffects/yellow.mp3'),
+    "blue": new Audio('./soundeffects/blue.mp3')
+};
 
-function addRandomNumbers() {
-
-    for (i = 0; i < 3; i++) {
-        computerChoices.push(GAMECOLORS[Math.floor(Math.random() * 3)])
-    }
-
-}
 
 addRandomNumbers()
+
 levelElement.innerText = `Level: ${level}`
+
+
+// Functions
 
 function userPlay(event) {
     userChoices.push(event.target.id)
@@ -28,6 +31,7 @@ function userPlay(event) {
     }
     else if (lose === false && userChoices.length === computerChoices.length){
         messageElement.innerText = "Passed the Level!!"
+        levelUp()
     }
     console.log(lose)
     console.log(userChoices.length)
@@ -35,43 +39,78 @@ function userPlay(event) {
 
 }
 
-function evaluateRound(){
-    for(i=0;i<userChoices.length;i++){
-        if(userChoices[i] != computerChoices[i]){
-            lose = true
-        }
-    }
-    return lose
-}
-
-
-for (const gameSquare of gameSquares) {
-    gameSquare.addEventListener("click", userPlay)
-}
-
-startButton.addEventListener("click", playSequence)
-
-function playSequence() {
-    messageElement.innerText = "Simon Says!"
-    let delay = 0
-    for (const choice of computerChoices) {
-        setTimeout(function () {
-           animateSquare(choice)
-        }, delay)
-        delay += 1000
-    }
-    setTimeout(function (){
-        messageElement.innerText = "Your Turn! :)"
-    }, 1000 * computerChoices.length)
-    console.log(computerChoices)
-}
-
 function animateSquare(selector){
     setTimeout(function () {
         const currentSquare = document.querySelector(`#${selector}`)
+        audio[`${selector}`].play()
         currentSquare.classList.toggle("get-rounder")
+        
         setTimeout(function () {
             currentSquare.classList.toggle("get-rounder")
         }, 500)})
     }
 
+    function playSequence() {
+        if (startButton){
+            startButton.remove()
+        }
+
+        messageElement.innerText = "Simon Says!"
+        let delay = 1000
+        for (const choice of computerChoices) {
+            setTimeout(function () {
+               animateSquare(choice)
+            }, delay)
+            delay += 1000
+        }
+        setTimeout(function (){
+            messageElement.innerText = "Your Turn! :)"
+        }, 1000 * (computerChoices.length + 1))
+        console.log(computerChoices)
+        for(const gameSquare of gameSquares){
+            gameSquare.style.cursor = "pointer"
+        }
+    }
+
+
+    function evaluateRound(){
+        for(i=0;i<userChoices.length;i++){
+            if(userChoices[i] != computerChoices[i]){
+                lose = true
+            }
+        }
+        return lose
+    }
+
+    function levelUp(){
+        level +=1
+        levelElement.innerText = `Level: ${level}`
+        levelElement.classList.toggle("text-grow")
+        setTimeout(function () {levelElement.classList.toggle("text-grow")}, 500)
+        userChoices = []
+        addRandomNumbers()
+        setTimeout(function (){
+            playSequence()
+        }, 2000)
+        
+        
+
+
+    }
+
+    function addRandomNumbers() {
+
+        for (i = 0; i < 1; i++) {
+            computerChoices.push(GAMECOLORS[Math.floor(Math.random() * 3)])
+        }
+    
+    }
+
+
+    // Event Listeners
+
+    for (const gameSquare of gameSquares) {
+        gameSquare.addEventListener("click", userPlay)
+    }
+    
+    startButton.addEventListener("click", playSequence)
